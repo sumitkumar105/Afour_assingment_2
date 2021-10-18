4593e645b9ea00d151d6c62cc0c35f776b280ca2
Sujitkumar Deshpande <sumitdeshpande105@gmail.com>
2021-10-18 15:44:20 +0530

    added

--- src/Cards/Card.js
+++ src/Cards/Card.js
@@ -1,39 +1,20 @@
-import React from 'react';
+import React from "react";
 import "../Cards/Card.css";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 const Card = () => {
-    return (
-      <div className="main">
-        <div className="card">
-          <div className="section">
-            <h1>fetching Data</h1>
-  
-            <button className="btn"><NavLink to="/get">get</NavLink></button>
-          </div>
+  return (
+    <div className="main">
+      <div className="card">
+        <div className="section">
+          <h1>fetching Data</h1>
+
+          <button className="btn">
+            <NavLink to="/get">get</NavLink>
+          </button>
         </div>
-        {/* <div className="card">
-          <div className="section">
-            <h1>Posting Data</h1>
-  
-             <button className="btn"><NavLink to="/post">post</NavLink></button>
-          </div>
-        </div> */}
-        {/* <div className="card">
-          <div className="section">
-            <h1>Put Data</h1>
-  
-            <button className="btn"><NavLink to="/post">post</NavLink></button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="section">
-            <h1>Delete Data</h1>
-  
-            <button className="btn">delete</button>
-          </div>
-        </div> */}
       </div>
-    );
-  };
-  
-  export default Card;
+    </div>
+  );
+};
+
+export default Card;
